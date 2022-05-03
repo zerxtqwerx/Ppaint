1a20b435fdffb052029b75e3b18d1d9b45e07b7c
Diana <78277090+zerxtqwerx@users.noreply.github.com>
2022-05-03 15:48:06 +0300

    Update Form1.cs

--- Ppaint/Form1.cs
+++ Ppaint/Form1.cs
@@ -10,18 +10,6 @@ using System.Windows.Forms;
 using System.Media;
 using System.IO;
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Media;
-using System.IO;
-
 namespace Ppaint
 {
     public partial class Ppaint : Form
